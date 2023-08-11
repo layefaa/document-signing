60944a1ef9ab1e7f14ecb49a47e1b9b33d38c9c0
Martin-Luther Obahor <kingm4159@gmail.com>
2023-08-11 16:30:39 +0100

    remove comments

--- src/data/index.ts
+++
@@ -1,3 +0,0 @@
-const Api_Url = "https://dev-api.gettonote.com/api/v1"
-
-export default Api_Url
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,29 +1,3 @@
-export interface IMenuContext {
-    isOpen: boolean,
-    toggleMenu: () => void
-}
-
-export interface ISocialIcon {
-    strokeColor: string,
-    dir?: string
-}
-
-export interface IButton {
-    label: string,
-    onClick?: () => void
-    type: 'button' | 'tab' | 'submit'
-    active?: boolean,
-    isSuccess?: boolean
-    isLoading?: boolean
-    isError?: boolean
-}
-
-export interface ILink {
-    name: string
-    route: string
-    clickEvent?: () => void
-}
-
 export interface IInput {
     label: string
     type?: string
